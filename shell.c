7077864c7a3c981f9eb8c32586d7134dd4dadea2
Ayse <ayse@example.com>
2025-12-06 11:23:29 +0000

    Complete Simple shell 0.1: working implementation

--- shell.c
+++ shell.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Simple main function
- * Return: 0
+ * main - Simple shell version 0.1
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("($) ");
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	while (1)
+	{
+		printf("($) ");
+		read = getline(&line, &len, stdin);
+
+		if (read == -1)
+		{
+			printf("\n");
+			break;
+		}
+
+		printf("%s", line);
+	}
+
+	free(line);
 	return (0);
 }
